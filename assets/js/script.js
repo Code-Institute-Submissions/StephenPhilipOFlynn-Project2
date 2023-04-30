771bb3a7f6df1e4b6e735b7bc8284be93ecf1448
Stephen OFlynn <soflynn@tcd.ie>
2023-04-30 20:49:34 +0000

    adding JS array of countries and capitals

---
+++ assets/js/script.js
@@ -0,0 +1,88 @@
+const countries = [
+    {name: "Brazil", capital: "Brasilia"},
+    {name: "Azerbaijan", capital: "Baku"},
+    {name: "Albania", capital: "Tirana"},
+    {name: "Algeria", capital: "Algiers"},
+    {name: "Australia", capital: "Canberra"},
+    {name: "Argentina", capital: "Buenos Aires"},
+    {name: "Belarus", capital: "Minsk"},
+    {name: "Bulgaria", capital: "Sofia"},
+    {name: "Bolivia", capital: "Sucre"},
+    {name: "Chile", capital: "Santiago"},
+    {name: "China", capital: "Beijing"},
+    {name: "Costa Rica", capital: "San Jose"},
+    {name: "Ivory Coast", capital: "Yamoussoukro"},
+    {name: "Czech Republic", capital: "Prague"},
+    {name: "Egypt", capital: "Cairo"},
+    {name: "Ecuador", capital: "Quito"},
+    {name: "Estonia", capital: "Tallinn"},
+    {name: "Ethiopia", capital: "Addis Ababa"},
+    {name: "Fiji", capital: "Suva"},
+    {name: "Finland", capital: "Helsinki"},
+    {name: "France", capital: "Paris"},
+    {name: "Greece", capital: "Athens"},
+    {name: "Germany", capital: "Berlin"},
+    {name: "Ghana", capital: "Accra"},
+    {name: "Guyana", capital: "Georgetown"},
+    {name: "Haiti", capital: "Port-Au-Prince"},
+    {name: "Honduras", capital: "Tegucigalpa"},
+    {name: "Hungary", capital: "Budapest"},
+    {name: "Iceland", capital: "Reykjavik"},
+    {name: "India", capital: "New Delhi"},
+    {name: "Italy", capital: "Rome"},
+    {name: "Israel", capital: "Jerusalem"},
+    {name: "Ireland", capital: "Dublin"},
+    {name: "Iraq", capital: "Baghdad"},
+    {name: "Iran", capital: "Tehran"},
+    {name: "Jamaica", capital: "Kingston"},
+    {name: "Japan", capital: "Tokyo"},
+    {name: "Jordan", capital: "Amman"},
+    {name: "India", capital: "New Delhi"},
+    {name: "Kazakhstan", capital: "Nur-Sultan"},
+    {name: "Kenya", capital: "Nariobi"},
+    {name: "Kosovo", capital: "Pristina"},
+    {name: "Kuwait", capital: "Kuwait City"},
+    {name: "Latvia", capital: "Riga"},
+    {name: "Lebanon", capital: "Beirut"},
+    {name: "Libya", capital: "Tripoli"},
+    {name: "Liberia", capital: "Monrovia"},
+    {name: "Morocco", capital: "Rabat"},
+    {name: "the Netherlands", capital: "Amsterdam"},
+    {name: "Norway", capital: "Oslo"},
+    {name: "Oman", capital: "Muscat"},
+    {name: "Pakistan", capital: "Islamabad"},
+    {name: "Panama", capital: "Panama City"},
+    {name: "Peru", capital: "Lima"},
+    {name: "Portugal", capital: "Lisbon"},
+    {name: "Qatar", capital: "Doha"},
+    {name: "Russia", capital: "Moscow"},
+    {name: "Slovakia", capital: "Bratislava"},
+    {name: "Spain", capital: "Madrid"},
+    {name: "Syria", capital: "Damascus"},
+    {name: "Thailand", capital: "Bangkok"},
+    {name: "Tanzania", capital: "Dodoma"},
+    {name: "Turkey", capital: "Ankara"},
+    {name: "Ukraine", capital: "Kiev"},
+    {name: "United States", capital: "Washington, D.C."},
+    {name: "Uruguay", capital: "Montevideo"},
+    {name: "Vietnam", capital: "Hanoi"},
+    {name: "Zimbabwe", capital: "Harare"},
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
