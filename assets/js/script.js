4c5a394a4ae1c67ea6f0aa60f4f3a983314a88fa
Stephen OFlynn <soflynn@tcd.ie>
2023-05-07 22:06:25 +0000

    syntax errors in questions array corrected

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,201 +1,201 @@
 const questions = [
     {
-    question: "Ireland"
+    question: "Ireland",
     answers: [
     { text: "Wellington", correct: false},
     { text: "Glasgow", correct: false},
     { text: "Dublin", correct: true},
     { text: "Ottawa", correct: false},
-]
+],
 },
 {
-    question: "Egypt"
+    question: "Egypt",
     answers: [
     { text: "Taipei", correct: false},
     { text: "Bratislava", correct: false},
     { text: "Dakar", correct: false},
     { text: "Cairo", correct: true},
-]
+],
 },
 {
-    question: "Iran"
+    question: "Iran",
     answers: [
     { text: "Tehran", correct: true},
     { text: "Beirut", correct: false},
     { text: "Islamabad", correct: false},
     { text: "Jerusalem", correct: false},
-    ]
+    ],
 },
 {
-    question: "Kosovo"
+    question: "Kosovo",
     answers: [
     { text: "Tehran", correct: false},
     { text: "Pristina", correct: true},
     { text: "Sofia", correct: false},
     { text: "Brussels", correct: false},
-    ]
+    ],
 },
 {
-    question: "Mauritius"
+    question: "Mauritius",
     answers: [
     { text: "Belmopan", correct: false},
     { text: "Mogadishu", correct: false},
     { text: "Port Louis", correct: true},
     { text: "Luanda", correct: false},
-    ]
+    ],
 },
 {
-    question: "Colombia"
+    question: "Colombia",
     answers: [
     { text: "Havana", correct: false},
     { text: "Bogata", correct: true},
     { text: "Caracas", correct: false},
     { text: "Banjul", correct: false},
-    ]
+    ],
 },
 {
-    question: "Azerbaijan"
+    question: "Azerbaijan",
     answers: [
     { text: "Baku", correct: true},
     { text: "Podgorica", correct: false},
     { text: "Belgrade", correct: false},
     { text: "Pretoria", correct: false},
-    ]
+    ],
 },
 {
-    question: "Mongolia"
+    question: "Mongolia",
     answers: [
     { text: "Berlin", correct: false},
     { text: "Minsk", correct: false},
     { text: "Ulaanbaatar", correct: true},
     { text: "Amman", correct: false},
-    ]
+    ],
 },
 {
-    question: "Costa Rica"
+    question: "Costa Rica",
     answers: [
     { text: "Havana", correct: false},
     { text: "Bern", correct: false},
     { text: "Port Moresby", correct: false},
     { text: "St Jose", correct: true},
-    ]
+    ],
 },
 {
-    question: "Thailand"
+    question: "Thailand",
     answers: [
     { text: "Bangkok", correct: true},
     { text: "Yaounde", correct: false},
     { text: "Rangoon", correct: false},
     { text: "Tunis", correct: false},    
-    ]
+    ],
 },
 {
-    question: "Nigeria"
+    question: "Nigeria",
     answers: [
     { text: "Nairobi", correct: false},
     { text: "Abuja", correct: true},
     { text: "Conakry", correct: false},
     { text: "Majuro", correct: false},    
-    ]
+    ],
 },
 {
-    question: "Jamaica"
+    question: "Jamaica",
     answers: [
     { text: "Rabat", correct: false},
     { text: "Libreville", correct: false},
     { text: "Porto-Novo", correct: false},
     { text: "Kingston", correct: true},    
-    ]
+    ],
 },
 {
-    question: "Chile"
+    question: "Chile",
     answers: [
     { text: "Lisbon", correct: false},
     { text: "Santiago", correct: true},
     { text: "Kampala", correct: false},
     { text: "Montevideo", correct: false},    
-    ]
+    ],
 },
 {
-    question: "Sweden"
+    question: "Sweden",
     answers: [
     { text: "Stockholm", correct: true},
     { text: "Oslo", correct: false},
     { text: "Helsinki", correct: false},
     { text: "Harare", correct: false},    
-    ]
+    ],
 },
 {
-    question: "Croatia"
+    question: "Croatia",
     answers: [
         { text: "Algiers", correct: false},
         { text: "Yerevan", correct: false},
         { text: "Tripoli", correct: false},
         { text: "Zagreb", correct: true},
-    ]
+    ],
 },
 {
-    question: "Peru"
+    question: "Peru",
     answers: [
         { text: "Victoria", correct: false},
         { text: "Lima", correct: true},
         { text: "Managua", correct: false},
         { text: "Porto-Novo", correct: true},
-    ]
+    ],
 },
 {
-    question: "South Korea"
+    question: "South Korea",
     answers: [
         { text: "Suva", correct: false},
         { text: "Beijing", correct: false},
         { text: "Pyongyang", correct: false},
         { text: "Seoul", correct: true},
-    ]
+    ],
 },
 {
-    question: "Saudi Arabia"
+    question: "Saudi Arabia",
     answers: [
         { text: "Riyadh", correct: true},
         { text: "San Salvador", correct: false},
         { text: "Damascus", correct: false},
         { text: "Beirut", correct: false},
-    ]
+    ],
 },
 {
-    question: "Australia"
+    question: "Australia",
     answers: [
         { text: "Lome", correct: false},
         { text: "Melbourne", correct: false},
         { text: "Canberra", correct: true},
         { text: "Banjul", correct: false},
-    ]
+    ],
 },
 {
-    question: "Uganda"
+    question: "Uganda",
     answers: [
         { text: "Kampala", correct: true},
         { text: "Doha", correct: false},
         { text: "Thimphu", correct: false},
         { text: "Moroni", correct: false},
-    ]
+    ],
 },
 {
-    question: "Slovakia"
+    question: "Slovakia",
     answers: [
         { text: "Honiara", correct: false},
         { text: "Malabo", correct: false},
         { text: "Prague", correct: false},
         { text: "Bratislava", correct: true},
-    ]
+    ],
 },
 {
-    question: "France"
+    question: "France",
     answers: [
         { text: "Berlin", correct: false},
         { text: "Paris", correct: true},
         { text: "Vienna", correct: false},
         { text: "Dublin", correct: false},
-    ]
+    ],
 },
 ]
 
@@ -251,4 +251,3 @@ startQuiz();
 
 
 
-]
